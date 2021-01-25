c704ff9453e1f6b41280f0d580529dfb74a72b0c
Corentin <corentin@blinkink.co.uk>
2021-01-25 18:48:48 +0000

    ✨ New Git JPS task

--- jeanpaulstart/tasks/git_.py
+++ jeanpaulstart/tasks/git_.py
@@ -1,23 +1,32 @@
+import git
+import os
 from jeanpaulstart.constants import *
 
-
 TASK_COMMAND = 'git'
-
+DEFAULT_BRANCH = 'master'
 
 def validate(user_data):
     return OK, ""
 
 
 def normalize_after_split(splitted):
-    splitted['arguments']['sub_directory'] = splitted['arguments'].get('sub_directory', None)
+    splitted['arguments']['branch'] = splitted['arguments'].get('branch', DEFAULT_BRANCH)
     return splitted
 
 
-def apply_(url, destination, sub_directory):
-    checkout_remote(url, destination, sub_directory)
+def apply_(url, dest, branch):
+    checkout_remote(url, dest, branch)
 
     return OK
 
 
-def checkout_remote(url, destination, sub_directory):
-    pass
+def checkout_remote(url, dest, branch):
+    if not os.path.exists(dest):
+        repo = git.Repo.clone_from(url, dest)
+    else:
+        repo = git.Repo(dest)
+
+    repo.git.reset("--hard")
+    repo.git.clean("-df")
+    repo.git.pull("origin", branch)
+    repo.git.checkout(branch)
